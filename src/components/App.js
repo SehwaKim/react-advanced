f17a7d3d47da83e0327d07afcd5221faf98a11d7
piper.kim <piper.kim@kakaoix.com>
2019-10-11 22:04:01 +0900

    react redux study

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+  return (
+  <div className="ui container">
+    <div>
+      <PostList/>
+    </div>
+  </div>);
+};
+
+export default App;
